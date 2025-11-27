e95fcdefbb6fc3682e7d9fa0396aeb36902386fb
IyaChris <ichristie@smu.edu>
2025-11-26 22:58:42 -0600

    iteration3

---
+++ utils/weather-client.mjs
@@ -0,0 +1,9 @@
+export class WeatherClient{
+    static #API_KEY = '';
+    static #LATITUDE = 32.8412178;
+    static #LONGITUDE = -96.7845175;
+    static #UNITS = 'imperial';
+    static #BASE_URL = https://api.openweathermap.org/data/2.5/weather;
+
+    
+}
