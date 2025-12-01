c7aac852a93aa723d6d0824389fe5840769c47a9
IyaChris <ichristie@smu.edu>
2025-12-01 09:54:31 -0600

    iteration4

--- public/main.mjs
+++ public/main.mjs
@@ -7,7 +7,7 @@ async function loadWeather () {
 console.log("Weather data:", data);
       const temp  = data.temp;
 
-        document.querySelector("#temp").textContent = temp !==undefined ? `${temp}f`: "NA";
+        document.querySelector("#temp").textContent = temp !==undefined ? `${temp}   degrees`: "NA";
     } catch (err) {
         console.error("Weather error", err);
     }
